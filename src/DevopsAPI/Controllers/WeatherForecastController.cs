0c448f0d07d02ba819f4509957660909ec85e504
Michael Murray <Michael.Murray@momentum.co.za>
2019-10-02 10:33:51 +0200

    fixed testing

--- src/DevopsAPI/Controllers/WeatherForecastController.cs
+++ src/DevopsAPI/Controllers/WeatherForecastController.cs
@@ -39,7 +39,7 @@ namespace DevopsAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Mikes";
+            return "Mike";
         } 
     }
 }
